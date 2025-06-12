1c7a1bc0329f515424fadf74b59b9790d26efeb4
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-06-12 14:45:54 +0530

    "Start the app basic "

---
+++ 01basicReactApp/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <h1>Learning react with vite | Divyesh Gandhi</h1>;
+}
+
+export default App;
