3343bdab72a02e7ae07b1c0803822a89afef7dc9
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-06-13 11:22:10 +0530

    Learned React component naming rules and file structure

---
+++ 01basicReactApp/src/Hey.jsx
@@ -0,0 +1,11 @@
+function Hey() {
+  return <h1>understand react flow and structure</h1>;
+}
+/*1.If you're creating a React component, then the function name MUST start with a capital letter.Otherwise, React will treat it as a normal HTML tag and won't render it.
+
+2. File extension is .js – is that an error?
+Nope — using .js is completely fine for both regular JavaScript and React component files.However, if you're writing React with JSX syntax (like <div>, <MyComponent />), then it's recommended (but not required) to use .jsx extension for clarity.
+
+*/
+
+export default Hey;
--- 01basicReactApp/src/main.jsx
+++ 01basicReactApp/src/main.jsx
@@ -1,9 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import Hey from "./Hey.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <>
+    <Hey />
+  </>
+);
