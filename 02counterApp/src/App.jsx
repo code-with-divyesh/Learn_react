5c92e5675feeaaba8a9ddf8ac8c014a9727a18c3
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-06-13 14:30:39 +0530

    Implemented counter app with useState and added range limit (0-20) with explanation

---
+++ 02counterApp/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+
+// ❌ Using a normal variable (like let counter = 0) doesn't update the UI when changed
+// ✅ useState is used in React to store and update values that should re-render the UI when changed
+
+  let [counter, setcounter] = useState(0);
+  // let counter = 15;
+
+  function addValue() {
+    if (counter < 20) {
+      counter = counter + 1;
+      setcounter(counter);
+      console.log("clicked", counter);
+    }
+  }
+  function removeValue() {
+    if (counter > 0) {
+      counter = counter - 1;
+      setcounter(counter);
+      console.log("clicked", counter);
+    }
+  }
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>Counter value:{counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  );
+}
+
+export default App;
